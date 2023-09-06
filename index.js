95debf26240bd9e9e8e69e1158b7ed9850dbdc1f
aryanbhat <aryanbhat324@gmail.com>
2023-09-06 23:10:05 +0530

    added endpoint to open the pdf

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const cors = require("cors");
 const port = 3000 || process.env.PRT;
 const fs = require("fs");
 const axios = require("axios");
-const FormData = require('form-data');
+const FormData = require("form-data");
 let formData = new FormData();
-
+const path = require('path')
 app.use(cors());
 app.use(express.json());
 
 let pdfList = [];
 
-fs.readdir('./Archive',(err,files)=>{
-  if(err){
+fs.readdir("./Archive", (err, files) => {
+  if (err) {
     console.log(err);
-  }
-  else{
-    const pdfs = files.map((file,id)=>{
-      return({
-        id:id,
-        name:change(file.split('.')[0].toString()),
-        path:"./Archive/"+file
-      });
-    })
+  } else {
+    const pdfs = files.map((file, id) => {
+      return {
+        id: id,
+        name: change(file.split(".")[0].toString()),
+        path: "./Archive/" + file,
+      };
+    });
     pdfList = [...pdfs];
   }
 });
@@ -49,39 +48,48 @@ app.get("/files", (req, res) => {
   res.json(pdfList);
 });
 
+app.get("/pdf/:id", (req, res) => {
+  const id = req.params.id;
+  for (let char of pdfList) {
+    if (char.id == id) {
+      const filePath = path.join(__dirname , char.path);
+      return res.sendFile(filePath);
+    }
+  }
+  res.status(401).json({"message":"Invalid params"});
+});
 
-app.post('/send/:id',(req,res)=>{
+app.post("/send/:id", (req, res) => {
   const id = req.params.id;
   var pdf = null;
-  for(let char of pdfList){
-    if(char.id == id){
+  for (let char of pdfList) {
+    if (char.id == id) {
       pdf = char;
     }
   }
-  if(pdf === null){
-    res.json({message:"Invalid Id"}).status(401);
+  if (pdf === null) {
+    res.json({ message: "Invalid Id" }).status(401);
     return;
   }
-  
-  let pdfPath = pdf.path;
-  formData.append('files', fs.createReadStream(pdfPath));
-  
-  
-  formData.append('name_space', pdf.name); 
-  
 
-  axios.post('https://gpt6-backend.onrender.com/create-namespace', formData, {
+  let pdfPath = pdf.path;
+  formData.append("files", fs.createReadStream(pdfPath));
+
+  formData.append("name_space", pdf.name);
+
+  axios
+    .post("https://gpt6-backend.onrender.com/create-namespace", formData, {
       headers: {
-          ...formData.getHeaders()
-      }
-  })
-  .then((response) => {
+        ...formData.getHeaders(),
+      },
+    })
+    .then((response) => {
       res.json(response.data);
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       res.json(error);
-  });
-})
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
